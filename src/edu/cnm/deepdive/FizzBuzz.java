e10ddd8f1106e5b2afe8000cb6cfc8c53770d719
Mickie Morlang <mickiemorlang@gmail.com>
2020-05-21 14:12:59 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/FizzBuzz.java
@@ -0,0 +1,22 @@
+package edu.cnm.deepdive;
+
+public class FizzBuzz {
+
+  public static void main(String[] args) {
+    for (int counter = 0; counter < 100; counter++) {
+      boolean counterIsFizz = (counter % 3 == 0);
+      boolean counterIsBuzz = (counter % 5 == 0);
+      if (counterIsFizz && counterIsBuzz) {
+        System.out.println("fizzbuzz");
+      } else if (counterIsFizz) {
+        System.out.println("fizz");
+      } else if (counterIsBuzz) {
+        System.out.println("buzz");
+      } else {
+        System.out.println(counter);
+      }
+    }
+
+  }
+
+  }
